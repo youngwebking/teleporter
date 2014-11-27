9ba8a58dcca8e67ba3c5559a16727cf531607470
Dave Powerman <davepowerman@gmail.com>
2014-11-27 13:27:33 -0500

    disable hrecord

--- app/models/product_hbase.rb
+++ app/models/product_hbase.rb
@@ -1,5 +1,5 @@
-class ProductHBase < HRecord::Base
-  server_url 'http://hbase.buildyourpatent.com'
-  set_tablename 'products'
-  column_families 'attr', 'img', 'file'
-end
+#class ProductHBase < HRecord::Base
+#  server_url 'http://hbase.buildyourpatent.com'
+#  set_tablename 'products'
+#  column_families 'attr', 'img', 'file'
+#end
