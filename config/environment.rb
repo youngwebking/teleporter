56893f71b33b07def4394437d175c3d0ed5d460f
Dave Powerman <davepowerman@gmail.com>
2014-11-27 13:23:03 -0500

    disable hrecord

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'hrecord/connection'
+#require 'hrecord'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-$hbase = HRecord::Connection.new 'http://hbase.davepowerman.com'
+#$hbase = HRecord::Connection.new 'http://hbase.davepowerman.com'
